14be5356a99018cabf6aad3b4b3173725024ac2e
rohanrana-agva <127920936+rohanrana-agva@users.noreply.github.com>
2023-04-02 14:21:36 +0530

    Add files via upload

---
+++ build/precache-manifest.ade32c4ca91cef85d47d8a5da721e357.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a2d1c46afc752fddaf",
+    "url": "/static/css/main.92a48c8c.chunk.css"
+  },
+  {
+    "revision": "73a2d1c46afc752fddaf",
+    "url": "/static/js/main.ba73cfca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a0dd96f6dec4a4a1e792",
+    "url": "/static/css/2.a5faa2fe.chunk.css"
+  },
+  {
+    "revision": "a0dd96f6dec4a4a1e792",
+    "url": "/static/js/2.3d0c18df.chunk.js"
+  },
+  {
+    "revision": "eff21cf5317e8328fda511410b2f1072",
+    "url": "/static/media/background.eff21cf5.png"
+  },
+  {
+    "revision": "b79c9928f65e4d912cbec3c67cfc0931",
+    "url": "/static/media/AlarmIcon.b79c9928.png"
+  },
+  {
+    "revision": "eb45ce4d020c54b89d83ac0509564d89",
+    "url": "/static/media/sidebar-vertical.eb45ce4d.png"
+  },
+  {
+    "revision": "f4fa235ecf93924acbccf5f95c1694f3",
+    "url": "/static/media/ddLoader.f4fa235e.gif"
+  },
+  {
+    "revision": "077726b1cd8736009f4edf94e844fafc",
+    "url": "/static/media/lgnewsmall.077726b1.png"
+  },
+  {
+    "revision": "26f4eaa69d5603098e048b982066a989",
+    "url": "/static/media/LgLargeIcon.26f4eaa6.png"
+  },
+  {
+    "revision": "b1c61800fb17db1be61458cd9b8a38ea",
+    "url": "/static/media/sunIcon.b1c61800.png"
+  },
+  {
+    "revision": "77f4d0a9b61586f85967af3163dab24d",
+    "url": "/index.html"
+  }
+];
